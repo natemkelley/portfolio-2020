ee3099f85a6add41a3b2d5be7728a4d0d3d504a5
nmkelley <natemkelley@gmail.com>
2020-03-11 13:06:11 -0700

    Update

--- pages/pages/Intro.vue
+++ pages/pages/Intro.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-small test-component">
-    <h1 class="text-center">Awesome Content</h1>
+    <h1 class="text-center">Nice Clicking</h1>
     <div class="bar"></div>
-    <p>Works like magic! By clicking on the buttons you will get access to more in depth explanations about some of my experiences.</p>
+    <p>By clicking on the buttons you will get access to more in depth explanations about some of my experiences.</p>
     <div class="me-cheering bouncing">
       <svg-icon :name="nateCheering" width="200px" height="200px" />
     </div>
